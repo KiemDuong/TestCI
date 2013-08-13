3de350d81e3c195ca4c32af501e59990b151c9b4
Duong Van Kiem <dv.kiem@gloops.com>
2013-08-13 16:25:38 +0700

    test Ci

---
+++ TestCI/Assets/Editor/EditorScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EditorScript
+{
+	 static string APP_NAME = "TestCI";
+	 
+	 static void PerformBuild ()
+     {
+		 string target_dir = APP_NAME + ".apk";
+         string[] scenes = { "Assets/JumpPlatform2.unity" };
+		
+		 // Test build on Android
+         BuildPipeline.BuildPlayer(scenes, target_dir, BuildTarget.Android, BuildOptions.None);
+     }
+}
