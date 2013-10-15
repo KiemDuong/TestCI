aa9e1e8d9927d5ce4e81aa89a379e691e0464d23
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-15 17:58:09 +0700

    ~ test build

--- Assets/Editor/EditorScript.cs
+++ Assets/Editor/EditorScript.cs
@@ -10,26 +10,9 @@ public class EditorScript
 	 static void PerformBuild ()
      {
 		string target_dir = APP_NAME;
-         string[] scenes = { "Assets/Scenes/TestCI.unity" };
+		string[] scenes = { "Assets/Scenes/TestCI.unity" };
 		
-		 // Test build 
-         BuildPipeline.BuildPlayer(scenes, target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
-		
-		// Create test suite
-        TestSuite suite = new TestSuite();
-
-        // Example: Add tests to suite
-        suite.AddAll(new Dummy_Test());
-
-        // Run the tests
-        TestResult res = suite.Run(null);
-
-        // Report results
-        Unity3D_TestReporter reporter = new Unity3D_TestReporter();
-        reporter.LogResults(res);
-
-		XML_TestReporter xmlReporter = new XML_TestReporter();
-		xmlReporter.Init("CI_test.xml");
-		xmlReporter.LogResults(res);
+		// Test build 
+		BuildPipeline.BuildPlayer(scenes, target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
      }
 }
