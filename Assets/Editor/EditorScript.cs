f8778dcd4893649dbb109b0cfb251b291acbede1
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-15 17:47:36 +0700

    ~ test report

--- Assets/Editor/EditorScript.cs
+++ Assets/Editor/EditorScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using SharpUnit;
 
 public class EditorScript
 {
@@ -14,6 +15,21 @@ public class EditorScript
 		 // Test build 
          BuildPipeline.BuildPlayer(scenes, target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
 		
-		
+		// Create test suite
+        TestSuite suite = new TestSuite();
+
+        // Example: Add tests to suite
+        suite.AddAll(new Dummy_Test());
+
+        // Run the tests
+        TestResult res = suite.Run(null);
+
+        // Report results
+        Unity3D_TestReporter reporter = new Unity3D_TestReporter();
+        reporter.LogResults(res);
+
+		XML_TestReporter xmlReporter = new XML_TestReporter();
+		xmlReporter.Init("CI_test.xml");
+		xmlReporter.LogResults(res);
      }
 }
