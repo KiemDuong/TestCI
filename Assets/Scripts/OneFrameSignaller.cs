c0b09d6a4044d0e81d49fe0217f6bee9dd1ab3e0
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-16 09:32:12 +0700

    ~ testt

--- Assets/Scripts/OneFrameSignaller.cs
+++ Assets/Scripts/OneFrameSignaller.cs
@@ -8,14 +8,17 @@ public class OneFrameSignaller : MonoBehaviour
 {
     public static bool atLeastOneFrameRan = false;
     public static DateTime startTime;
-    public static TimeSpan testDuration;    
+    public static TimeSpan testDuration; 
+	public static bool isFinished = false;
     
     void Update()
     {
         atLeastOneFrameRan = true;
 //        StartCoroutine(signalFrameEnd()); // use this to quit after this frame, you could use a counter instead
-        if (DateTime.Now - OneFrameSignaller.startTime > OneFrameSignaller.testDuration) // use this if you want a timed end of the test
+        if (!isFinished && DateTime.Now - OneFrameSignaller.startTime > OneFrameSignaller.testDuration) // use this if you want a timed end of the test
         {
+			isFinished = true;
+			
 			StartCoroutine(signalFrameEnd());
         }
 		// Thread.Sleep(20); //this is somewhat evil, us this to extend the execution time 
@@ -29,7 +32,7 @@ public class OneFrameSignaller : MonoBehaviour
     }
     
     IEnumerator signalFrameEnd()
-    {
+    {	
 		Debug.LogError ("AAAAA!");
         yield return new WaitForEndOfFrame();
 		Debug.LogError ("BBBBB!");
