5cbca44868fe117b1b43760221d9586c5ae2fe24
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-18 11:42:15 +0700

    !!! Re-structure project

---
+++ Assets/1_AutomaticBuild/Editor/AutomaticBuildScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AutomaticBuildScript
+{
+	 static string APP_NAME = "TestCI";
+	
+	 ///////////////////////////////////////////////////////////////////////////////////////////////////
+	 static void PerformBuild (BuildTarget target, BuildOptions option = BuildOptions.None)
+     {
+		EditorUserBuildSettings.SwitchActiveBuildTarget (target);
+		
+		string target_dir = APP_NAME;
+		string[] scenes = { "Assets/Scenes/TestBuild.unity" };
+		
+		// Test build 
+		BuildPipeline.BuildPlayer(scenes, target_dir, target, option);
+     }
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static void PerformBuildAndroid ()
+	{		
+		AutomaticBuildScript.PerformBuild (BuildTarget.Android);
+	}
+	
+	static void PerformBuildWindows ()
+	{
+		AutomaticBuildScript.PerformBuild (BuildTarget.StandaloneWindows);
+	}
+	
+	static void PerformBuildMAC ()
+	{
+		AutomaticBuildScript.PerformBuild (BuildTarget.StandaloneOSXIntel);
+	}
+	
+	static void PerformBuildIOS ()
+	{
+		AutomaticBuildScript.PerformBuild (BuildTarget.iPhone);
+	}
+}
---
+++ Assets/2_UnitTest/Editor/UnitTestRunScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class UnitTestRunScript : MonoBehaviour {
+
+	static void PerformTest () {
+		
+		EditorApplication.OpenScene("Assets/Scenes/TestCI_NUnit.unity");	
+		
+		GameObject oneFrameObject = new GameObject ("OneFrameObject");
+
+		oneFrameObject.AddComponent <OneFrameSignaller> ();
+		
+		EditorApplication.isPlaying = true;
+	}
+	
+	[MenuItem("UnitTest/PerformTest (Windows)")]
+	static void PerformTestWindows () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.StandaloneWindows);
+		
+		UnitTestRunScript.PerformTest ();
+	}
+	
+	[MenuItem("UnitTest/PerformTest (MAC)")]
+	static void PerformTestMAC () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.StandaloneOSXIntel);
+		
+		UnitTestRunScript.PerformTest ();
+	}
+	
+	[MenuItem("UnitTest/PerformTest (Android)")]
+	static void PerformTestAndroid () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.Android);
+		
+		UnitTestRunScript.PerformTest ();
+	}
+	
+	[MenuItem("UnitTest/PerformTest (iOS)")]
+	static void PerformTestIOS () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.iPhone);
+		
+		UnitTestRunScript.PerformTest ();
+	}
+}
