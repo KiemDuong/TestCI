b092d291dcbe20049f9bf2f54dcbc23426708bf8
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-18 13:42:22 +0700

    !!! clean up project before making a build

--- Assets/Automatic/1_AutomaticBuild/Editor/AutomaticBuildScript.cs
+++ Assets/Automatic/1_AutomaticBuild/Editor/AutomaticBuildScript.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
+using System.IO;
+
 public class AutomaticBuildScript
 {
 	 static string APP_NAME = "TestCI";
 	
 	 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	 static void PerformBuild (BuildTarget target, BuildOptions option = BuildOptions.None)
-     {
+	{	
+		// Clean up
+		Directory.Delete ("Assets/Automatic/2_UnitTest", true);
+		
 		EditorUserBuildSettings.SwitchActiveBuildTarget (target);
 		
 		string target_dir = APP_NAME;
