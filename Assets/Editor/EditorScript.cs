105d00424b0abaceb92d4a6d13dd826f4c4494e4
nh.phuong@gloops.com <nh.phuong@gloops.com>
2013-10-18 09:47:07 +0700

    !! Build project

--- Assets/Editor/EditorScript.cs
+++ Assets/Editor/EditorScript.cs
@@ -6,17 +6,19 @@ using SharpUnit;
 public class EditorScript
 {
 	 static string APP_NAME = "TestCI";
-	 
-	 static void PerformBuild ()
+	
+	 ///////////////////////////////////////////////////////////////////////////////////////////////////
+	 static void PerformBuild (BuildTarget target, BuildOptions option = BuildOptions.None)
      {
+		EditorUserBuildSettings.SwitchActiveBuildTarget (target);
+		
 		string target_dir = APP_NAME;
 		string[] scenes = { "Assets/Scenes/TestCI.unity" };
 		
 		// Test build 
-		BuildPipeline.BuildPlayer(scenes, target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes, target_dir, target, option);
      }
 	
-	[MenuItem("Window/PerformTest")]
 	static void PerformTest () {
 		
 		EditorApplication.OpenScene("Assets/Scenes/TestCI_NUnit.unity");	
@@ -26,5 +28,58 @@ public class EditorScript
 		oneFrameObject.AddComponent <OneFrameSignaller> ();
 		
 		EditorApplication.isPlaying = true;
+	}
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static void PerformBuildAndroid ()
+	{		
+		EditorScript.PerformBuild (BuildTarget.Android);
+	}
+	
+	static void PerformBuildWindows ()
+	{
+		EditorScript.PerformBuild (BuildTarget.StandaloneWindows);
+	}
+	
+	static void PerformBuildMAC ()
+	{
+		EditorScript.PerformBuild (BuildTarget.StandaloneOSXIntel);
+	}
+	
+	static void PerformBuildIOS ()
+	{
+		EditorScript.PerformBuild (BuildTarget.iPhone);
+	}
+	
+	[MenuItem("Window/PerformTest (Windows)")]
+	static void PerformTestWindows () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.StandaloneWindows);
+		
+		EditorScript.PerformTest ();
+	}
+	
+	[MenuItem("Window/PerformTest (MAC)")]
+	static void PerformTestMAC () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.StandaloneOSXIntel);
+		
+		EditorScript.PerformTest ();
+	}
+	
+	[MenuItem("Window/PerformTest (Android)")]
+	static void PerformTestAndroid () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.Android);
+		
+		EditorScript.PerformTest ();
+	}
+	
+	[MenuItem("Window/PerformTest (iOS)")]
+	static void PerformTestIOS () {
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.iPhone);
+		
+		EditorScript.PerformTest ();
 	}
 }
